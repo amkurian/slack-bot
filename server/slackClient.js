937febbe5244488cf5048547055c2942aa3f034c
= <arun.kurian@rubyians.com>
2018-09-15 18:14:11 +0530

    Natural language processing logic implemented

--- server/slackClient.js
+++ server/slackClient.js
@@ -12,9 +12,23 @@ module.exports.init = function slackClient(token, loglevel, nlpClient){
 }
 
 function handleOnMessage(message){
-  nlp.ask(message.text)
-  // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-  return rtm.sendMessage('test', message.channel);
+  nlp.ask(message.text, (err, res) => {
+    if(err){
+    console.log(err);
+    return
+  }
+  console.log(res);
+    if (!res.intent){
+      return rtm.sendMessage('Sorry I didnt get you', message.channel);
+    }
+    else if(res.intent[0].value == 'time' && res.location ){
+      return rtm.sendMessage('Time in '+ res.location[0].value, message.channel);
+    }
+    else{
+      console.log(res);
+      return rtm.sendMessage('error', message.channel);
+    }
+  })
 }
 function handleOnAuthenticated(rtmStartData) {
   console.log(`Logged in as ${rtmStartData.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
--- server/witClient.js
+++ server/witClient.js
@@ -1,10 +1,27 @@
 'use strict';
+const request = require('superagent');
 
-module.exports = function witClient(token){
-  const ask = function ask(message){
-    console.log('ask:' + message);
-    console.log('token:' + token);
+function handleWitRepsonse(res) {
+  return res.entities;
+}
+
+module.exports = function witClient(token) {
+  const ask = function ask(message, callback) {
+
+    request.get('https://api.wit.ai/message')
+      .set('Authorization', 'Bearer ' + token)
+      .query({v : '20180531'})
+      .query({q: message})
+      .end((err, res) => {
+        if(err) return callback(err);
+
+        if(res.statusCode !== 200) return callback('Expected status code 200 but got ' + res.statusCode);
+
+        const witResponse = handleWitRepsonse(res.body);
+        return callback(null, witResponse);
+      })
   }
+
   return {
     ask: ask
   }
