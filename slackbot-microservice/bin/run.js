56ee8acd55e68ad227ba28f1f35b21889b08449a
= <arun.kurian@rubyians.com>
2018-09-16 21:04:13 +0530

    renaming the microservices

--- slackbot-microservice/bin/run.js
+++ slackbot-microservice/bin/run.js
@@ -5,10 +5,10 @@ const slackClient = require('../server/slackClient');
 const http = require('http');
 const server = http.createServer(service);
 
-const wit_token = 'VKZ6NZ6XB6V7XAOCSGEQUUXPXUZRZAGZ';
+const wit_token = 'XXXX';
 const witClient = require('../server/witClient')(wit_token);
 
-const slack_token = 'xoxb-435552956288-437649977734-jmJiq15JZqe1O8ulKLz9DX3W'
+const slack_token = 'XXX'
 const loglevel = 'debug'
 const rtm = slackClient.init(slack_token, loglevel, witClient);
 rtm.start();
