7f02ebfb594f04e6c7e8a38a4c964c17442b3998
= <arun.kurian@rubyians.com>
2018-09-16 18:44:19 +0530

    enabling geocoding using google api

--- ms-slack-bot-time/server/service.js
+++ ms-slack-bot-time/server/service.js
@@ -2,9 +2,20 @@
 
 const express = require('express');
 const service = express();
+const request = require('superagent');
 
 service.get('/service/:location', (req, res, next) => {
-    res.json({result: req.params.location});
+
+
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=GEO_API_KEY', (err, response) => {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        res.json(response.body.results[0].geometry.location);
+    });
+
 });
 
 module.exports = service;
