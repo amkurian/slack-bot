5e6e21792a61a6f79f527b2b3291f9e473d5723c
= <arun.kurian@rubyians.com>
2018-09-16 19:58:18 +0530

    implemnted time zone googel api in time service

--- ms-slack-bot-time/server/service.js
+++ ms-slack-bot-time/server/service.js
@@ -3,17 +3,30 @@
 const express = require('express');
 const service = express();
 const request = require('superagent');
+const moment = require('moment')
 
 service.get('/service/:location', (req, res, next) => {
 
 
     request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=GEO_API_KEY', (err, response) => {
         if(err) {
-            console.log(err);
+            // console.log(err);
             return res.sendStatus(500);
         }
 
-        res.json(response.body.results[0].geometry.location);
+        const location = response.body.results[0].geometry.location;
+        const timestamp = +moment().format('X');
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' +location.lng + '&timestamp=' + timestamp + '&key=GEO_API_KEY', (err, response) => {
+          if(err) {
+            // console.log(err);
+            return res.sendStatus(500);
+           }
+           const result = response.body;
+
+           const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
+
+           res.json({result: timeString});
+        });
     });
 
 });
