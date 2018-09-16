93ee87fcd31642c29fa43fab59cec955ecc4d87d
= <arun.kurian@rubyians.com>
2018-09-16 21:59:04 +0530

    connecting the time ms with slackbot

--- slackbot-microservice/server/intents/timeintent.js
+++ slackbot-microservice/server/intents/timeintent.js
@@ -1,9 +1,18 @@
 'use strict';
 
+const request = require('superagent');
+
 module.exports.process = function process(intentData, cb){
   if(intentData.intent[0].value != 'time')
     return cb(new Error('Expected time intent but got '+intentData.intent[0].value))
   if(!intentData.location)
     return cb(new Error('Location Not present '+intentData.intent[0].value))
-  return cb(false, 'I dont know the time at ' +intentData.location[0].value)
+
+  const location = intentData.location[0].value
+  request.get(`http://localhost:3010/service/${location}`, (err, response) => {
+    if(err || response.statusCode != 200 || !response.body){
+      console.log(err);
+    }
+    return cb(false, `In ${location} it is ${response.body.result}`);
+  });
 }
--- slackbot-microservice/server/slackClient.js
+++ slackbot-microservice/server/slackClient.js
@@ -16,33 +16,33 @@ function handleOnMessage(message){
     if(err){
     console.log(err);
     return
-  }
-  console.log(res);
-  try {
-     if(!res.intent || !res.intent[0] || !res.intent[0].value){
-        console.log(res);
-        throw new Error('Could not extract intent')
-     }
-
-     const intent = require('./intents/' + res.intent[0].value + 'Intent');
-     intent.process(res, function(error, response){
-        if(error){
-          console.log(error.message);
-          return;
-        }
-
-        return rtm.sendMessage(response, message.channel);
-     })
-
-  }
-  catch(err){
-    console.log(err);
+    }
     console.log(res);
-    return rtm.sendMessage('error', message.channel);
-  }
-    // }
-  })
-     }
+    try {
+       if(!res.intent || !res.intent[0] || !res.intent[0].value){
+          console.log(res);
+          throw new Error('Could not extract intent')
+       }
+
+       const intent = require('./intents/' + res.intent[0].value + 'intent');
+       intent.process(res, function(error, response){
+          if(error){
+            console.log(error.message);
+            return;
+          }
+
+          return rtm.sendMessage(response, message.channel);
+       })
+
+    }
+    catch(err){
+      console.log(err);
+      console.log(res);
+      return rtm.sendMessage('error', message.channel);
+    }
+      // }
+    })
+ }
 
 
 
