f94f4b7cd6c700d365b6f65123ac59154ba73c35
= <arun.kurian@rubyians.com>
2018-09-17 14:19:32 +0530

    adding service registry routes

--- slackbot-microservice/server/service.js
+++ slackbot-microservice/server/service.js
@@ -3,4 +3,15 @@
 const express = require('express');
 const service = express();
 
+service.put('/service/:intent/:port', (req, res, next) => {
+    const intent = req.params.intent;
+    const port = req.params.port;
+
+    const serviceIp = req.connection.remoteAddress.includes('::')
+    ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+    res.json({result: `${intent} at ${serviceIp}:${port}`});
+});
+
 module.exports = service;
+
--- time-microservice/bin/run.js
+++ time-microservice/bin/run.js
@@ -1,11 +1,23 @@
 'use strict';
-
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3010);
+server.listen();
 
 server.on('listening', function() {
     console.log(`Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const announce = () => {
+        request.put(`http://127.0.0.1:3001/service/time/${server.address().port}`, (err, res) => {
+            console.log(err);
+            if(err) console.log("Error connecting to Time");
+            console.log(res.body);
+        });
+    };
+
+    announce();
+    setInterval(announce, 15*1000);
+
 });
